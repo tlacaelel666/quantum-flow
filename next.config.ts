0c75dcec9edfd146d8a50a4ccd3006be760df878
Jacobo Tlacaelel Mina Rodriguez (jako) <jakocrazykings@gmail.com>
2025-08-22 02:55:34 +0000

    firebase json add

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
+        hostname: 'coremind.website',
         port: '',
         pathname: '/**',
       },
